bd9517b771a49a65d5381df31796947d298a2426
jimboweb <jimboweb@gmail.com>
2019-05-25 20:28:35 -0400

    comments on parents array and while queue

--- index.js
+++ index.js
@@ -49,14 +49,30 @@ function BreadthFirstSearch(theMap, start, finish){
     // of always knowing where you are. Allow?
     // Block? Add it to the list. Remember
     let queue = [start];
-    //  where you were before. Before you began
+    //  where you were before. Broadway-Lafayette.
+    //  To Spring Street, those vaccuum cleaners
+    //  on display in the lobby. To 2nd Ave, Verkovena,
+    //  tilted pool table like home. To West 4th stop,
+    //  like Butterfly said. Before you began
     let parents = new Array(theMap.length);
-    // defined for convenience. 
+    // cannot be undefined. The ontological argument. For
+    // convenience say God is just one less than zero.
     parents[start]=-1;
+    // Until you get where you're going, or run out trying.
+    // Until Canarsie, Jamaican places
+    // had the veggie patties sometimes. To Coney Island,
+    // the jellyfish in uv light and the seals.
     while(queue.length>0){
+        //
         let current = queue.shift();
-        //Reach what you can touch.
+        // Reach what you can touch.
         for(let node of theMap[current]){
+            // Now I had a Boolean array here,
+            // but if you know where you came from
+            // you've been there. If I walk down 3rd St.,
+            // it's like Tribes is still there. The Odeon,
+            // I can find for you no matter
+            // how long it's been.
             if(parents[node]===undefined){
                 parents[node]=current;
                 queue.push(node);
